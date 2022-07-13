1213299670fa773f778c71ea9f0d9ab1063d2740
Will Oswald <57539054+williameoswald@users.noreply.github.com>
2022-07-13 13:25:49 +0100

    Update

--- R/compare_survey.R
+++ R/compare_survey.R
@@ -83,7 +83,7 @@ compare_survey <- function(form1, form2, language){
   cat(crayon::bold(crayon::blue(print("Check 3 - Compare type per name\n"))))
   types_different <- compare %>%
     filter(type.x!=type.y) %>%
-    select(type.x,type.y,name)
+    select(name,type.x,type.y)
 
   if (nrow(types_different)>0) {
     print("Discrepancies exist in type for fields with same name:")
